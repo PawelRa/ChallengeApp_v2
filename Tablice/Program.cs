e8137dad2f7d25b83e3660fa7c88161975c50284
PawelRa <pawelrat@poczta.onet.pl>
2023-10-05 19:01:32 +0200

    tablice i listy

---
+++ Tablice/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] grades = new int[365];
+string[] daysOfWeeks = new string[7];
+
+daysOfWeeks[0] = "poniedzialek";
+daysOfWeeks[1] = "wtorek";
+daysOfWeeks[2] = "środa";
+daysOfWeeks[3] = "czwartek";
+daysOfWeeks[4] = "piątek";
+daysOfWeeks[5] = "sobota";
+daysOfWeeks[6] = "niedziela";
+
+//Console.WriteLine(daysOfWeeks[2]);
+
+string[] dayOfWeeks2 = { "poniedzialek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
+
+//Console.WriteLine(dayOfWeeks2[5]);
+
+for (int i = 0; i < dayOfWeeks2.Length; i++)
+{
+    Console.WriteLine(dayOfWeeks2[i]);
+}
+
+// Listy
+List<string> days = new List<string>();
+days.Add("poniedzialek");
+days.Add("wtorek");
+days.Add("środa");
+days.Add("czwartek)";
+days.Add("piątek");
+days.Add("sobota");
+days.Add("niedziela");
