16da09a06d67f2111cf0a880cb91bc816f922946
PawelRa <pawelrat@poczta.onet.pl>
2023-10-06 22:10:31 +0200

    Zadanie domowe - dzień 6

---
+++ Dzien 6 - zadanie domowe/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace Dzien_6___zadanie_domowe
+{
+    public class Employee
+    {
+        private List<int> scores = new List<int>();
+
+        public Employee(string name, string surname, int age) {
+            this.Age = age; 
+            this.Name = name; 
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result { get {  return scores.Sum(); } }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+    }
+}
--- Dzień 6 - lekcje/Program.cs
+++ Dzień 6 - lekcje/Program.cs
@@ -1,4 +1,5 @@
-﻿// Klasy
+﻿using Dzień_6___lekcje;
+// Klasy
 
 User user1 = new User("Adam", "0123456789");
 User user2 = new User("Zuzia", "0123456789");
@@ -11,30 +12,5 @@ user1.AddScore(2);
 var result = user1.Result;
 Console.WriteLine(result);
 
-class User
-{
-    private List<int> score = new List<int>();
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-    }
-
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-    }
-}
-
-
+var gameName = User.GameName;
+var pi = Math.PI;
