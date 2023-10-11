06cf64e2b898a18e44be5a05ea6294a885daca2f
PawelRa <pawelrat@poczta.onet.pl>
2023-10-11 15:08:37 +0200

    przerwa

---
+++ ChallengeApp_v2/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace ChallengeApp_v2
+{
+    public class Employee
+    {
+        private List<float> grades = new List<float>();
+
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
+        {
+            this.grades.Add(grade);
+        }
+    }
+}
